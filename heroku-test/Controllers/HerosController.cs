38c0e3bb766fe43b73fdafb7853f309184e711c7
muatheJamil <muathe@trigger-apps.com>
2022-04-13 21:05:32 +0300

    second commit

--- heroku-test/Controllers/HerosController.cs
+++ heroku-test/Controllers/HerosController.cs
@@ -5,7 +5,7 @@ namespace heroku_test.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class HerosController : ControllerBase
+    public class UsersController : ControllerBase
     {
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> Get()
@@ -13,9 +13,21 @@ namespace heroku_test.Controllers
             var heros = new List<SuperHero>
             { new SuperHero {
                 Id = 1,
-                Name = "Spider Man",
+                Name = "Ahmad",
+                FirstName = "Tamer",
+                SecondName = "Luis",
+                Place = "New York City"},
+                new SuperHero {
+                Id = 2,
+                Name = "Monther",
+                FirstName = "Arafat",
+                SecondName = "Mother",
+                Place = "Ramallah"},
+                new SuperHero {
+                Id = 2,
+                Name = "Samer",
                 FirstName = "Peter",
-                SecondName = "Tamer",
+                SecondName = "Meme",
                 Place = "New York City"}
             };
 
