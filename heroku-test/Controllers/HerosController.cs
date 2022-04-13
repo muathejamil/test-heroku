b8367631507dc0748401de289db209205b2bbf77
muatheJamil <muathe@trigger-apps.com>
2022-04-13 20:49:51 +0300

    first commit

---
+++ heroku-test/Controllers/HerosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace heroku_test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HerosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            var heros = new List<SuperHero>
+            { new SuperHero {
+                Id = 1,
+                Name = "Spider Man",
+                FirstName = "Peter",
+                SecondName = "Tamer",
+                Place = "New York City"}
+            };
+
+            return Ok(heros);
+        }
+    }
+}
---
+++ heroku-test/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace heroku_test
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string FirstName { get; set; } = String.Empty;
+        public string SecondName { get; set; } = String.Empty;
+        public string Place{ get; set; } = String.Empty;
+    }
+}
